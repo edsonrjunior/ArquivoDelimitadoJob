1d01c5d68957df20a585089791a70e5231863a43
Edson Junior <edsonkjr@gmail.com>
2023-06-09 13:27:03 -0300

    Subindo projeto para o repo

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,13 @@
+
+Ñ
+Tsrc/main/java/com/springbatch/arquivodelimitado/ArquivoDelimitadoJobApplication.java,e\7\e704457807d878fa07cc543d049f5f98b5f8a64c
+t
+Dsrc/main/java/com/springbatch/arquivodelimitado/dominio/Cliente.java,1\9\1999fed8a05f8b9b2d57c9a0b29e62dca6588f18
+å
+\src/main/java/com/springbatch/arquivodelimitado/step/LeituraArquivoDelimitadoStepConfig.java,e\5\e52b62cdfc834d6e37786b0ba4738c675459ba21
+É
+Ssrc/main/java/com/springbatch/arquivodelimitado/job/ArquivoDelimitadoJobConfig.java,f\0\f0a83537c4d9e9ed22e9b4adb6f4fe3a480443ca
+ê
+`src/main/java/com/springbatch/arquivodelimitado/reader/LeituraArquivoDelimitadoReaderConfig.java,9\d\9d3a73df1b838ca9955245fd7f627ceac51316fc
+ê
+`src/main/java/com/springbatch/arquivodelimitado/writer/LeituraArquivoDelimitadoWriterConfig.java,8\a\8a4975631599fe88ddb3b545c65f56761c166c14
---
+++ src/main/java/com/springbatch/arquivodelimitado/reader/LeituraArquivoDelimitadoReaderConfig.java
@@ -0,0 +1,27 @@
+package com.springbatch.arquivodelimitado.reader;
+
+import com.springbatch.arquivodelimitado.dominio.Cliente;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+
+@Configuration
+public class LeituraArquivoDelimitadoReaderConfig {
+
+    @StepScope
+    @Bean
+    public FlatFileItemReader<Cliente> leituraArquivoDelimitadoReader(@Value("#{jobParameters['arquivoClientes']}") Resource arquivoClientes) {
+        return new FlatFileItemReaderBuilder<Cliente>()
+                .name("leituraArquivoDelimitadoReader")
+                .resource(arquivoClientes)
+                .encoding("UTF-8")
+                .delimited()// O padrão é ","
+                .names(new String[]{"nome", "sobrenome", "idade", "email"})
+                .targetType(Cliente.class)
+                .build();
+    }
+}
